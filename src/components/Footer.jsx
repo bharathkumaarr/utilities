d8b708cda687404fdf5527661994b5a5b6ee4f17
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-07-20 00:32:47 +0530

    footer ui improvements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,8 @@ function Footer() {
           <span className=''>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#737373" viewBox="0 0 256 256"><path d="M214.75,211.71l-62.6-98.38,61.77-67.95a8,8,0,0,0-11.84-10.76L143.24,99.34,102.75,35.71A8,8,0,0,0,96,32H48a8,8,0,0,0-6.75,12.3l62.6,98.37-61.77,68a8,8,0,1,0,11.84,10.76l58.84-64.72,40.49,63.63A8,8,0,0,0,160,224h48a8,8,0,0,0,6.75-12.29ZM164.39,208,62.57,48h29L193.43,208Z"></path></svg>
           </span>
-           @<a className='hover:underline' href="https://x.com/bharathkumxr/" target='_blank'>bharathkumxr</a></p>
-        <p className='border p-2 pl-3 pr-3 rounded-full text-center flex items-center justify-center hover:bg-neutral-600 transition-colors duration-300 ease-in-out hover:text-neutral-900'><a href="https://github.com/bharathkumaarr/utilities" target='_blank'>Feel free to contribute here</a></p>
+           <a className='underline hover:text-neutral-400' href="https://x.com/bharathkumxr/" target='_blank'>@bharathkumxr</a></p>
+        <p className='border p-2 pl-3 pr-3 rounded-full text-center flex items-center justify-center hover:bg-neutral-400 transition-colors duration-300 ease-in-out hover:text-neutral-900 hover:border-neutral-300'><a href="https://github.com/bharathkumaarr/utilities" target='_blank'>Feel free to contribute here</a></p>
     </CardFooter>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,7 @@ function Hero() {
     <>
     {/* <UuidGenerator /> */}
     
+    
 
     </>
   )
